ef97754428485e1ad1a6ed0924413927d544f776
Doug Burke <dburke@cfa.harvard.edu>
2011-12-19 19:42:19 -0500

    Updated cabal to 0.08; moved code around in line2pdf.hs

--- line2pdf.hs
+++ line2pdf.hs
@@ -4,21 +4,27 @@ import System.Environment
 import Text.LineToPDF
 import qualified Data.ByteString.Lazy.Char8 as L
 
+import Control.Monad (when)
+
+usage :: IO ()
+usage = do
+  putStrLn "Usage: line2pdf [-big5|-gbk|-shiftjis|-euc-jp|-euc-kr] input.txt > output.pdf"
+  putStrLn "  (Form feed (^L) in input denotes a pagebreak.)"
+  exitWith ExitSuccess
+  
 main :: IO ()
 main = do
     args <- getArgs
+    when (null args) usage
 
-    (enc, input) <- case args of
-        []      -> do
-            putStrLn "Usage: line2pdf [-big5|-gbk|-shiftjis|-euc-jp|-euc-kr] input.txt > output.pdf"
-            putStrLn "  (Form feed (^L) in input denotes a pagebreak.)"
-            exitWith ExitSuccess
-        ("-big5":i:_)     -> return (Big5, i)
-        ("-gbk":i:_)      -> return (GBK, i)
-        ("-euc-jp":i:_)   -> return (EUC_JP, i)
-        ("-euc-kr":i:_)   -> return (EUC_KR, i)
-        ("-shiftjis":i:_) -> return (ShiftJIS, i)
-        (i:_)             -> return (Latin, i)
+    let (enc, input) = case args of
+          ("-big5":i:_)     -> (Big5, i)
+          ("-gbk":i:_)      -> (GBK, i)
+          ("-euc-jp":i:_)   -> (EUC_JP, i)
+          ("-euc-kr":i:_)   -> (EUC_KR, i)
+          ("-shiftjis":i:_) -> (ShiftJIS, i)
+          (i:_)             -> (Latin, i)
+          []                -> error "usage error" -- quieten -Wall
 
     src <- L.readFile input
     lineToPDF $ defaultConfig enc src
